c87ede251a3867fa948dfef047076eb024a9078c
Selva73582 <102418337+Selva73582@users.noreply.github.com>
2024-03-20 12:30:35 +0530

    Delete base directory

--- base/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-from . import models
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=models.Product
-        fields="__all__"
